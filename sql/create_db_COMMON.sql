8393f7c2366b6c8dbaa550abacf6f6dd7de95ff2
Steve Jones <testmonger@users.noreply.github.com>
2017-07-25 20:02:19 -0500

    Update create_db_COMMON.sql

--- sql/create_db_COMMON.sql
+++ sql/create_db_COMMON.sql
@@ -4,7 +4,7 @@ See https://github.com/testmonger/db_test_tools
 */
 USE master;
 
-IF  NOT EXISTS (SELECT * FROM sys.databases WHERE name like 'Common' )
-	CREATE DATABASE [Common];
+IF  NOT EXISTS (SELECT * FROM sys.databases WHERE name like 'CommonTools' )
+	CREATE DATABASE [CommonTools];
 
 GO
